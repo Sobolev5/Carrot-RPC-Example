6e590ecafcf9a23a1ed778b82273ad60b232c5b6
Andrey Sobolev <email.asobolev@gmail.com>
2022-10-25 20:25:17 +0000

    Sobolev:LastFixes

--- microservice_interface/app.py
+++ microservice_interface/app.py
@@ -27,10 +27,16 @@ html = """
                     carrot-rpc==0.2.5
                 </div>
                 <div class"card-body">   
-                    <div class="p-3">  
-                        Sum A and B will be calculated on microservice «microservice_sum» and validated with pydantic schema.
+                    <div class="p-3"> 
+                        1) This is page render by <code>MICROSERVICE_INTERFACE</code>
                         <br>
-                        <small><b>Note</b> This page render by «microservice_interface».</small>               
+                        2) Number A + Number B will be calculated on <code>MICROSERVICE_SUM</code> with pydantic schema validation and 
+                        <code>MICROSERVICE_INTERFACE</code> render response from <code>MICROSERVICE_SUM</code>
+                        <br>
+                        3) <code>MICROSERVICE_SUM</code> work with amqp and http protocols together.
+                        You can open http route of <code>MICROSERVICE_SUM</code> <a href="http://5.187.4.179:5889/">http://5.187.4.179:5889/</a>
+                        </small>      
+                        <hr>    
                         <form action="/" method="POST">
                             <div class="mb-3">
                                 <label class="form-label">Number A</label>
@@ -40,7 +46,7 @@ html = """
                                 <label class="form-label">Number B</label>
                                 <input type="number" class="form-control" name="number_b" value="10">
                             </div>
-                            <button type="submit" class="btn btn-primary">Sum A and B</button>
+                            <button type="submit" class="btn btn-primary">Calculate sum A and B on MICROSERVICE_SUM and return result to MICROSERVICE_INTERFACE</button>
                         </form>
                     </div>
                 </div>
@@ -53,7 +59,7 @@ html = """
 # defer main function which call microservice «microservice_sum»:
 async def call_sum_a_and_b(request):
     if request.method == "POST":
-        sprint(f"call_sum_a_and_b", с="green", s=1, p=1)
+        sprint(f"call_sum_a_and_b", с="green", p=1, f=1)
         data = await request.form()
 
         # make dict request:
@@ -63,15 +69,19 @@ async def call_sum_a_and_b(request):
         dct["number_b"] = int(data["number_b"])
 
         # defer carrot instance and make rpc call:
-        carrot = await Carrot(AMQP_URI).connect()
-        response_from_another_microservice = await carrot.call(dct, "microservice_sum:sum_a_and_b")    
+        carrot = await Carrot(AMQP_URI, DEBUG=True).connect()
 
-        # dct: first arg is dict with data
-        # another_microservice:sum_a_and_b: second arg it routing key (through default AMQP exchange) 
+        response_from_another_microservice = await carrot.call(dct, "microservice_sum:sum_a_and_b", timeout=5)  
+        # first arg is dict with data
+        # second arg it routing key (through default AMQP exchange) 
+        # third arg is optional (response timeout in seconds, 5 seconds by default)       
 
-        # get response dict from microservice «microservice_sum»
-        sprint(f'Sum a and b: {response_from_another_microservice["sum"]}', c="yellow", s=1, p=1)
-        return JSONResponse({"sum (response from microservice_sum)": response_from_another_microservice["sum"]})    
+        if "error" in response_from_another_microservice:
+            return JSONResponse(response_from_another_microservice) 
+        else:  
+            # get response dict from microservice «microservice_sum»
+            sprint(f'Sum a and b: {response_from_another_microservice["sum"]}', c="yellow", f=1, p=1)
+            return JSONResponse({"sum (response from microservice_sum)": response_from_another_microservice["sum"]})    
     else:
         return HTMLResponse(html)
 
--- microservice_sum/app.py
+++ microservice_sum/app.py
@@ -15,14 +15,15 @@ AMQP_URI = "amqp://admin:YTmJqsNx2@rabbitmq/vhost"
 # make pydantic schema:
 class SumAAndB(BaseModel):
     caller: str
-    a: int
-    b: int
+    number_a: int
+    number_b: int
 
 # decorate called function with pydantic schema
 @carrot_ask(SumAAndB)
 async def sum_a_and_b(incoming_dict: dict) -> dict:
-    sprint(incoming_dict, c="yellow", s=1, p=1)
+    sprint(incoming_dict, c="yellow", f=1, p=1)
     dct = {}
+    #await asyncio.sleep(7) for test timeout
     dct["caller"] = "i am sum_a_and_b function mounted on microservice_sum"
     dct["sum"] = incoming_dict["number_a"] + incoming_dict["number_b"]
     return dct
@@ -31,7 +32,7 @@ async def sum_a_and_b(incoming_dict: dict) -> dict:
 async def amqp_router():
     connection = await aiormq.connect(AMQP_URI)
     channel = await connection.channel()
-    sprint(f"AMQP:     ready [yes]", c="green", s=1, p=1)
+    sprint(f"AMQP:     ready [yes]", c="green", f=1, p=1)
     sum_a_and_b__declared = await channel.queue_declare(f"microservice_sum:sum_a_and_b", durable=False)
     await channel.basic_consume(sum_a_and_b__declared.queue, sum_a_and_b, no_ack=False)  
     
@@ -45,17 +46,16 @@ async def amqp_router():
 http_router = APIRouter()
 
 # add http endpoint
-@http_router.post("/")
-async def main(request):
-    return {"http": "ready"}
+@http_router.get("/")
+async def main():
+    return {"hello": "i m microservice sum (this is my http part)"}
 
 ## HTTP END ###################################
 
 
 class App(FastAPI):
     def __init__(self, *args, **kwargs):
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
+        loop = asyncio.get_event_loop()
         loop.create_task(amqp_router())
         super().__init__(*args, **kwargs)
 
